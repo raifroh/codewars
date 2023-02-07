0123acc5caae32d74b693e9272317fccf314792c
Rai_Froh <raifroh@gmail.com>
2023-02-07 10:19:58 -0800

    commit

---
+++ 8kyu/convert-a-boolean-to-a-string.js
@@ -0,0 +1,9 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+//parameters a boolean
+//return boolean as string
+//example see test cases
+
+const booleanToString = b => b.toString()
