3666cacc13145a60e6608c0115ad2dda3e55dfce
Rai_Froh <raifroh@gmail.com>
2023-02-12 19:38:07 -0800

    commit

---
+++ 8kyu/super-duper-easy.js
@@ -0,0 +1,11 @@
+// //parameters a number ideally
+//return the number multiplied by 50 and +6 that num
+//example see below
+
+
+function problem(x){
+    if(typeof x === 'string'){
+      return 'Error'
+    }else( x = x*50 + 6)
+    return x
+  }
