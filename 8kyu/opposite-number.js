6aaa4024c69e5cbf0abce28a995c5b98e1c83fd7
Rai_Froh <raifroh@gmail.com>
2023-05-08 19:59:54 -0700

    commit

---
+++ 8kyu/opposite-number.js
@@ -0,0 +1,7 @@
+//parameters = number
+//return number with negative or without
+//example 5 to -5 and -5 to 5
+//psuedo code just see if number > 0 if so return number * -1
+
+
+const opposite = number => number * -1
